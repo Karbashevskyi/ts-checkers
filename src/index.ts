36ad1779a76e60f1efbfe04394a6ef43af682782
Ivan Karbashevskyi <ivan.karbashevskyi@unicard.pl>
2022-04-26 15:18:50 +0200

    0.0.3

--- src/index.ts
+++ src/index.ts
@@ -1,187 +1,173 @@
 export class Is {
+  /**
+   *
+   * @param value is any type
+   */
+  public static null(value: any): boolean {
+    return [null, 'null'].includes(value);
+  }
 
-    /**
-     *
-     * @param value is any type
-     */
-    public static null(value: any): boolean {
-        return [null, 'null'].includes(value);
+  /**
+   *
+   * @param value is any type
+   */
+  public static undefined(value: any): boolean {
+    return [undefined, 'undefined'].includes(value);
+  }
+
+  /**
+   *
+   * @param value is any type
+   */
+  public static nullOrUndefined(value: any): boolean {
+    return this.null(value) || this.undefined(value);
+  }
+
+  /**
+   *
+   * @param value is any type
+   */
+  public static notNullOrUndefined(value: any): boolean {
+    return !this.nullOrUndefined(value);
+  }
+
+  /**
+   *
+   * @param value is any type
+   */
+  public static number(value: any): boolean {
+    return typeof value === 'number' && !isNaN(value);
+  }
+
+  /**
+   *
+   * @param value is any type
+   */
+  public static notNumber(value: any): boolean {
+    return !this.number(value);
+  }
+
+  /**
+   * TODO add test
+   * @param value is any type
+   */
+  public static universalEmptyChecker(value: any): boolean {
+    if (this.nullOrUndefined(value)) {
+      return true;
     }
 
-    /**
-     *
-     * @param value is any type
-     */
-    public static undefined(value: any): boolean {
-        return [undefined, 'undefined'].includes(value);
+    if (this.number(value)) {
+      value = true;
+    } else {
+      value = Object.keys(value)?.length;
     }
 
-    /**
-     *
-     * @param value is any type
-     */
-    public static nullOrUndefined(value: any): boolean {
-        return this.null(value) || this.undefined(value);
+    return value === 0;
+
+    // TODO Add JSON.parse?
+  }
+
+  /**
+   * TODO add test
+   * @param value is object type
+   */
+  public static emptyObject(value: object): boolean {
+    return this.universalEmptyChecker(value);
+  }
+
+  /**
+   * TODO add test
+   * @param value is object type
+   */
+  public static notEmptyObject(value: object): boolean {
+    return !this.emptyObject(value);
+  }
+
+  /**
+   * TODO add test
+   * @param value is string type
+   */
+  public static emptyString(value: string): boolean {
+    return this.universalEmptyChecker(value);
+  }
+
+  /**
+   * TODO add test
+   * @param value is string type
+   */
+  public static notEmptyString(value: string): boolean {
+    return !this.emptyString(value);
+  }
+
+  /**
+   * TODO add test
+   * @param value is any type
+   */
+  public static nullOrUndefinedOrEmpty(value: any): boolean {
+    return this.universalEmptyChecker(value);
+  }
+
+  /**
+   * TODO add test
+   * @param value is any type
+   */
+  public static notNullOrUndefinedOrEmpty(value: any): boolean {
+    return !this.nullOrUndefinedOrEmpty(value);
+  }
+
+  /**
+   * TODO add test
+   * @param enabled is boolean type
+   */
+  public static notFalse(enabled: boolean): boolean {
+    return enabled !== false;
+  }
+
+  /**
+   * TODO add test
+   * @param objectOne is generic type
+   * @param objectTwo is generic type
+   */
+  public static notCompare<T>(objectOne: T, objectTwo: T): boolean {
+    return !this.compare(objectOne, objectTwo);
+  }
+
+  /**
+   * TODO add test
+   * @param objectOne is generic type
+   * @param objectTwo is generic type
+   */
+  public static compare<T>(objectOne: T, objectTwo: T): boolean {
+    if (this.nullOrUndefined(objectOne) || this.nullOrUndefined(objectTwo)) {
+      return false;
     }
 
-    /**
-     *
-     * @param value is any type
-     */
-    public static notNullOrUndefined(value: any): boolean {
-        return !this.nullOrUndefined(value);
+    const keysOfObjectOne: string[] = Object.keys(objectOne) ?? [];
+    const keysOfObjectTwo: string[] = Object.keys(objectTwo) ?? [];
+
+    if (!keysOfObjectOne?.length || !keysOfObjectTwo?.length) {
+      return false;
     }
 
-    /**
-     *
-     * @param value is any type
-     */
-    public static number(value: any): boolean {
-        return typeof value === 'number' && !isNaN(value);
+    if (keysOfObjectOne.length !== keysOfObjectOne.length) {
+      return false;
     }
 
-    /**
-     *
-     * @param value is any type
-     */
-    public static notNumber(value: any): boolean {
-        return !this.number(value);
-    }
-
-    /**
-     * TODO add test
-     * @param value is any type
-     */
-    public static universalEmptyChecker(value: any): boolean {
-
-        if (this.nullOrUndefined(value)) {
-            return true;
-        }
-
-        if (this.number(value)) {
-            value = true;
-        } else {
-            value = Object.keys(value)?.length;
-        }
-
-        return value === 0;
-
-        // TODO Add JSON.parse?
-
-    }
-
-    /**
-     * TODO add test
-     * @param value is object type
-     */
-    public static emptyObject(value: object): boolean {
-        return this.universalEmptyChecker(value);
-    }
-
-    /**
-     * TODO add test
-     * @param value is object type
-     */
-    public static notEmptyObject(value: object): boolean {
-        return !this.emptyObject(value);
-    }
-
-    /**
-     * TODO add test
-     * @param value is string type
-     */
-    public static emptyString(value: string): boolean {
-        return this.universalEmptyChecker(value);
-    }
-
-    /**
-     * TODO add test
-     * @param value is string type
-     */
-    public static notEmptyString(value: string): boolean {
-        return !this.emptyString(value);
-    }
-
-    /**
-     * TODO add test
-     * @param value is any type
-     */
-    public static nullOrUndefinedOrEmpty(value: any): boolean {
-        return this.universalEmptyChecker(value);
-    }
-
-    /**
-     * TODO add test
-     * @param value is any type
-     */
-    public static notNullOrUndefinedOrEmpty(value: any): boolean {
-        return !this.nullOrUndefinedOrEmpty(value);
-    }
-
-    /**
-     * TODO add test
-     * @param enabled is boolean type
-     */
-    public static notFalse(enabled: boolean): boolean {
-        return enabled !== false;
-    }
-
-    /**
-     * TODO add test
-     * @param objectOne is generic type
-     * @param objectTwo is generic type
-     */
-    public static notCompare<T>(objectOne: T, objectTwo: T): boolean {
-
-        return !this.compare(objectOne, objectTwo);
-
-    }
-
-    /**
-     * TODO add test
-     * @param objectOne is generic type
-     * @param objectTwo is generic type
-     */
-    public static compare<T>(objectOne: T, objectTwo: T): boolean {
-
-        if (this.nullOrUndefined(objectOne) || this.nullOrUndefined(objectTwo)) {
+    for (const keyOfObjectOne of keysOfObjectTwo) {
+      switch (typeof (objectOne as any)[keyOfObjectOne]) {
+        case 'object':
+          if (this.notCompare((objectOne as any)[keyOfObjectOne], (objectTwo as any)[keyOfObjectOne])) {
             return false;
-        }
-
-        const keysOfObjectOne: string[] = Object.keys(objectOne) ?? [];
-        const keysOfObjectTwo: string[] = Object.keys(objectTwo) ?? [];
-
-        if (!keysOfObjectOne?.length || !keysOfObjectTwo?.length) {
+          }
+          break;
+        default:
+          if ((objectOne as any)[keyOfObjectOne] !== (objectTwo as any)[keyOfObjectOne]) {
             return false;
-        }
-
-        if (keysOfObjectOne.length !== keysOfObjectOne.length) {
-
-            return false;
-
-        }
-
-        for (const keyOfObjectOne of keysOfObjectTwo) {
-
-            switch (typeof (objectOne as any)[keyOfObjectOne]) {
-                case 'object':
-                    if (this.notCompare((objectOne as any)[keyOfObjectOne], (objectTwo as any)[keyOfObjectOne])) {
-                        return false;
-                    }
-                    break;
-                default:
-                    if ((objectOne as any)[keyOfObjectOne] !== (objectTwo as any)[keyOfObjectOne]) {
-
-                        return false;
-
-                    }
-                    break;
-            }
-
-        }
-
-        return true;
-
+          }
+          break;
+      }
     }
 
+    return true;
+  }
 }
