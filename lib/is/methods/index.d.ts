235befa349586339c2190ae7d156397ebc8fac09
karba <ivankarbashevskyi@gmail.com>
2022-05-17 09:17:15 +0200

    After: build, test, lint

--- lib/is/methods/index.d.ts
+++ lib/is/methods/index.d.ts
@@ -1,18 +1,29 @@
+import { Compare } from './compare';
+import { EmptyObject } from './empty-object';
+import { EmptyString } from './empty-string';
+import { False } from './false';
+import { Null } from './null';
+import { NullOrUndefined } from './null-or-undefined';
+import { NullOrUndefinedOrEmpty } from './null-or-undefined-or-empty';
+import { Number } from './number';
+import { True } from './true';
+import { Undefined } from './undefined';
+import { UniversalEmptyChecker } from './universal-empty-checker';
 /**
  * Declare names and types of methods
  */
 export interface MethodsInterface {
-    Compare: Function;
-    EmptyObject: Function;
-    EmptyString: Function;
-    False: Function;
-    Null: Function;
-    NullOrUndefined: Function;
-    NullOrUndefinedOrEmpty: Function;
-    Number: Function;
-    True: Function;
-    Undefined: Function;
-    UniversalEmptyChecker: Function;
+    Compare: typeof Compare;
+    EmptyObject: typeof EmptyObject;
+    EmptyString: typeof EmptyString;
+    False: typeof False;
+    Null: typeof Null;
+    NullOrUndefined: typeof NullOrUndefined;
+    NullOrUndefinedOrEmpty: typeof NullOrUndefinedOrEmpty;
+    Number: typeof Number;
+    True: typeof True;
+    Undefined: typeof Undefined;
+    UniversalEmptyChecker: typeof UniversalEmptyChecker;
 }
 /**
  * Export all accesses methods in library
